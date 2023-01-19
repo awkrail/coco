ab7b37def9a2e4f3ca0fb0abe16bc4712b22c175
Taichi Nishimura <awkrail01@gmail.com>
2023-01-19 14:22:39 +0900

    bridge frame_buffer to kernel

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -8,6 +8,7 @@
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
 #include <Guid/FileInfo.h>
+#include "frame_buffer_config.h"
 
 struct MemoryMap {
   UINTN buffer_size;
@@ -218,6 +219,25 @@ EFI_STATUS EFIAPI UefiMain(
   for(UINTN i = 0; i < gop->Mode->FrameBufferSize; ++i) {
     frame_buffer[i] = 255;
   }
+  
+  struct FrameBufferConfig config = {
+    (UINT8*)gop->Mode->FrameBufferBase,
+    gop->Mode->Info->PixelsPerScanLine,
+    gop->Mode->Info->HorizontalResolution,
+    gop->Mode->Info->VerticalResolution,
+    0
+  };
+  switch (gop->Mode->Info->PixelFormat) {
+    case PixelRedGreenBlueReserved8BitPerColor:
+      config.pixel_format = kPixelRGBResv8BitPerColor;
+      break;
+    case PixelBlueGreenRedReserved8BitPerColor:
+      config.pixel_format = kPixelBGRResv8BitPerColor;
+      break;
+    default:
+      Print(L"Unimplemented pixel format: %d\n", gop->Mode->Info->PixelFormat);
+      Halt();
+  }
 
   // boot kernel
   EFI_FILE_PROTOCOL* kernel_file;
@@ -273,9 +293,9 @@ EFI_STATUS EFIAPI UefiMain(
   }
 
   UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
-  typedef void EntryPointType(UINT64, UINT64);
+  typedef void EntryPointType(const struct FrameBufferConfig*);
   EntryPointType* entry_point = (EntryPointType*)entry_addr;
-  entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
+  entry_point(&config);
 
   Print(L"All done\n");
 
--- kernel/main.c
+++ kernel/main.c
@@ -1,11 +1,55 @@
 #include <stdint.h>
+#include <stddef.h>
+#include "frame_buffer_config.h"
 
-void KernelMain(uint64_t frame_buffer_base,
-                uint64_t frame_buffer_size) {
-  uint8_t *frame_buffer = (uint8_t*)frame_buffer_base;
-  for(uint64_t i = 0; i < frame_buffer_size; ++i) {
-    frame_buffer[i] = i % 256;
+struct PixelColor 
+{
+  uint8_t r, g, b;
+};
+
+void WriteRGBPixel(const struct FrameBufferConfig *config,
+                  int x, int y, const struct PixelColor *c)
+{
+  const int pixel_position = config->pixels_per_scan_line * y + x;
+  uint8_t *p = &config->frame_buffer[4 * pixel_position];
+  p[0] = c->r;
+  p[1] = c->g;
+  p[2] = c->b;
+}
+
+void WriteBGRPixel(const struct FrameBufferConfig *config,
+                  int x, int y, const struct PixelColor *c)
+{
+  const int pixel_position = config->pixels_per_scan_line * y + x;
+  uint8_t *p = &config->frame_buffer[4 * pixel_position];
+  p[0] = c->b;
+  p[1] = c->g;
+  p[2] = c->r;
+}
+
+void KernelMain(const struct FrameBufferConfig *frame_buffer_config) 
+{
+  void (*WritePixel)(const struct FrameBufferConfig *config,
+                     int x, int y, const struct PixelColor *c);
+  if(frame_buffer_config->pixel_format == kPixelRGBResv8BitPerColor) {
+    WritePixel = WriteRGBPixel;
+  } else if (frame_buffer_config->pixel_format == kPixelBGRResv8BitPerColor) {
+    WritePixel = WriteBGRPixel;
   }
 
+  const struct PixelColor white = {255, 255, 255};
+  const struct PixelColor green = {0, 255, 0};
+
+  for(int x=0; x < frame_buffer_config->horizontal_resolution; ++x){
+    for(int y=0; y < frame_buffer_config->vertical_resolution; ++y) {
+      WritePixel(frame_buffer_config, x, y, &white);
+    }
+  }
+
+  for(int x=0; x < 200; ++x) {
+    for(int y=0; y<100; ++y) {
+      WritePixel(frame_buffer_config, 100+x, 100+y, &green);
+    }
+  }
   while (1) __asm__("hlt");
 }
